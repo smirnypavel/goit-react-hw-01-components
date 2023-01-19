2725464474577ecf5766f25b23a97e89502432cb
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-01-19 17:49:17 +0200

    profile done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { ProfileCard } from './Profile/ProfileCard/ProfileCard';
+import user from './Profile/ProfileCard/user.json'
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+ return (
+  <div>
+      <ProfileCard 
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+      // stats={user.stats}
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      />
+  </div>
+ );
 };
